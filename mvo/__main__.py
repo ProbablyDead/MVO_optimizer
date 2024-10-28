dcb3fd5b2f536c6fefe2e9a042884cd3adbebd2e
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2024-10-28 19:21:18 +0300

    add benchmark

--- mvo/__main__.py
+++ mvo/__main__.py
@@ -1,20 +1,28 @@
 from MVO_optimizer import MVO_optimizer
 import benchmarks
+import inspect
+from time import time
 
 
 def main():
-    func = benchmarks.F5
-    function_name, lb, up, dim = benchmarks.getFunctionDetails(func.__name__)
+    functions = sorted(list(
+        filter(lambda x: x[0].startswith("F"), inspect.getmembers(
+            benchmarks, inspect.isfunction))
+    ), key=lambda x: int(x[0][1:]))
 
-    optimizer = MVO_optimizer(
-        func, dim, lb, up)
+    for func in functions:
+        start_time = time()
+        func = func[1]
+        function_name, lb, up, dim = benchmarks.getFunctionDetails(
+            func.__name__)
 
-    best_solution, best_score = optimizer.optimize()
-    print('\n'.join([
-        f'{function_name=}',
-        f'{best_score=}',
-        f'{best_solution=}'])
-    )
+        optimizer = MVO_optimizer(
+            func, dim, lb, up, visualization=False, max_time=100000
+        )
+
+        best_solution, best_score = optimizer.optimize()
+        time_s = time() - start_time
+        print(f'{function_name=}\t{best_score=}\t{time_s=}')
 
 
 if __name__ == "__main__":
