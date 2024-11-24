c70cc3c9b1fde9dec23a0b65cc5472196f855e48
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2024-11-24 17:09:49 +0300

    add changes to Universe class

--- mvo/Universe.py
+++ mvo/Universe.py
@@ -1,5 +1,4 @@
 import numpy as np
-from random import uniform
 
 
 class Universe:
@@ -17,37 +16,35 @@ class Universe:
     def set_parameters(_, f=None, dim=None, lb=None, ub=None):
         Universe.__f = f if f else Universe.__f
         Universe.__dim = dim if dim else Universe.__dim
-        Universe.__lb = Universe.__check_bounds(lb) if lb else Universe.__lb
-        Universe.__ub = Universe.__check_bounds(ub) if ub else Universe.__ub
+        Universe.__lb = Universe.__check_bounds(
+            lb) if lb is not None else Universe.__lb
+        Universe.__ub = Universe.__check_bounds(
+            ub) if ub is not None else Universe.__ub
 
     def __init__(self):
-        assert all((
+        assert all(filter(lambda x: x != 0, (
             Universe.__f,
             Universe.__dim,
             Universe.__lb,
             Universe.__ub
-        )), "f, dim, lb, ub must be specified in the Universe class using set_parameters method"
+        ))), "f, dim, lb, ub must be specified in the Universe class using set_parameters method"
 
-        self.__array = np.array([uniform(Universe.__lb[i], Universe.__ub[i])
+        self.__array = np.array([np.random.uniform(Universe.__lb[i], Universe.__ub[i])
                                  for i in range(Universe.__dim)])
 
     def get_array(self):
         return self.__array
 
+    def get_parameter(self, at):
+        return self.__array[at]
+
     def get_inflation_rate(self):
+        assert self.__f, "f must be specified"
         return Universe.__f(self.__array)
 
-    def send(self, at, value):
-        self.__array[at] = value
+    def send(self, to, at, delta=0):
+        to.receive(at, self.__array[at] + delta)
 
-
-Universe.set_parameters(
-    f=(lambda x: np.sum(x**2)),
-    dim=2,
-    lb=[1, 10],
-    ub=[2, 20]
-)
-
-u = Universe()
-u.send(1, 100)
-print(u.get_array())
+    def receive(self, at, value):
+        self.__array[at] = np.clip(
+            value, Universe.__lb[at], Universe.__ub[at])
